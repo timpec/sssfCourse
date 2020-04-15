e685d80f10aabe421dac8e47e9c5cc6cad31080f
timpec <timpe96@hotmail.com>
2020-04-15 18:44:48 +0300

    first commit

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
