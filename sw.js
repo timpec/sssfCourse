b25e6869db81c9577c8cab3fd7805b5067f3e3c3
timpec <timpe96@hotmail.com>
2020-04-16 12:55:00 +0300

    first commit

--- sw.js
+++ sw.js
@@ -8,18 +8,26 @@ var filesToCache = [
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+  e.waitUntil((async () => {
+    try {
+      const cache = await caches.open(cacheName);
       return cache.addAll(filesToCache);
-    })
-  );
+    }
+    catch (e) {
+      console.log('after install', e.message);
+    }
+  })());
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
+  e.respondWith((async () => {
+    try {
+      const response = await caches.match(e.request);
       return response || fetch(e.request);
-    })
-  );
+    }
+    catch (e) {
+      console.log('load cache', e.message);
+    }
+  })());
 });
